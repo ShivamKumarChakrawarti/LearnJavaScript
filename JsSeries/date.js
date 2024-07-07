5e7d109f75240f068c840758cc9259a6e1286960
Shivam Kumar Chakrawarti <chakrawartishivam28@gmail.com>
2024-07-07 10:44:53 +0000

    Dates

---
+++ JsSeries/date.js
@@ -0,0 +1,14 @@
+let myDtate = new Date()
+// console.log(myDtate);
+
+// console.log(myDtate.toString());
+// console.log(myDtate.toDateString());
+// console.log(myDtate.toLocaleString());
+// console.log(myDtate.toLocaleDateString());
+// console.log(myDtate.toISOString());
+// console.log(myDtate.toJSON());
+// console.log(myDtate.toTimeString());
+// console.log(myDtate.getHours());
+
+// console.log(myDtate.setMonth(7));
+
