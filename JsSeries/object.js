6b6d8314135f7eb322707f5efd45ad75c92d7dd3
Shivam Kumar Chakrawarti <chakrawartishivam28@gmail.com>
2024-07-15 14:44:57 +0000

    objectUnderstanding

--- JsSeries/object.js
+++ JsSeries/object.js
@@ -9,9 +9,22 @@ const obj = {
     isLogined: false,
     location: "Pune"
 }
-console.log(obj.name);
-console.log(obj["name"]);
-console.log(obj.anotherName);
-console.log(obj["anotherName"]);
-console.log(obj["email"]);
-console.log(typeof obj[mySym]);
+// console.log(obj.name);
+// console.log(obj["name"]);
+// console.log(obj.anotherName);
+// console.log(obj["anotherName"]);
+// console.log(obj["email"]);
+// console.log(typeof obj[mySym]);
+
+// Destructuring of object
+const obj1 = {
+    course: "Java",
+    price: "70$",
+    courseInstructor: "KK"
+}
+
+console.log(obj1.courseInstructor);
+// If we have multiple statements and we want to optimize code then we use destructuring
+
+const {courseInstructor: instructor, course: C} = obj1
+console.log(instructor, C);//we dont need to write obj1.courseInstructor again again.
