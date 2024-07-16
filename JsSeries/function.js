4c2d4a0d8410903fd226a22ca1fd785991441ad7
Shivam Kumar Chakrawarti <chakrawartishivam28@gmail.com>
2024-07-16 14:32:15 +0000

    functionUnderstanding

--- JsSeries/function.js
+++ JsSeries/function.js
@@ -1,7 +1,7 @@
 function addTwoNumbers(a, b){
     return a+b
 }
-console.log(addTwoNumbers(10, 20));
+// console.log(addTwoNumbers(10, 20));
 
 function loginUserMessage(userName){
     if(!userName){
@@ -12,4 +12,31 @@ function loginUserMessage(userName){
 }
 
 // console.log(loginUserMessage());
-console.log(loginUserMessage("shivam"));
+// console.log(loginUserMessage("shivam"));
+
+// Function with Object
+const user = {
+    userName: "Shivam",
+    pswd: "abc@123"
+}
+
+function handelObject(obj) {
+    console.log(`your username is: ${obj.userName} and password is: ${obj.pswd}`);
+    
+}
+// console.log(handelObject(user));
+// Or
+handelObject({
+    userName: "ABC@gmail.com",
+    pswd: "abc@123"
+})
+
+// Function with Array
+const myArr = [10, 20, 30, 40, 50]
+
+function handelArray(arr){
+    return arr[2]
+}
+// console.log(handelArray(myArr));
+// Or
+console.log(handelArray([10, 20, 30, 40, 50]));
